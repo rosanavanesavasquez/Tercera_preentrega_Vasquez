231b2a726b6b1b8d9c76131b7fee779040f9ae31
Vasquezr <rosana.vanesa.vasquez@gmail.com>
2023-10-12 20:33:21 -0300

    Actualización con correcciones de mi tercera entrega

--- ProyectoAuditoria/AppAuditoria/admin.py
+++ ProyectoAuditoria/AppAuditoria/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from.models import *
 
-# Register your models here.
+# Acá registro los modelos y los tengo que importar para que los pueda usar. 
+# #Importo todo * pero podría seleccionar solo el modelo que quiero
+
+admin.site.register(Auditor)
+admin.site.register(Auditado)
+admin.site.register(Sector)
+admin.site.register(Entregable)
+
